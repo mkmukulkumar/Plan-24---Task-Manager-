e5a727de6ef7b77e39b90bdb139b10216eb96176
mkmukulkumar <mukulkumar258@gmail.com>
2021-08-06 00:45:38 +0530

    time to plan feature

--- app/src/main/java/com/babra/plan24/MainActivity.java
+++ app/src/main/java/com/babra/plan24/MainActivity.java
@@ -1,6 +1,7 @@
 package com.babra.plan24;
 
 import android.app.Dialog;
+import android.icu.text.DecimalFormat;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -20,6 +21,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,7 +31,7 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.Ite
     Button cancel_button, save_button;
     EditText popup_task_name;
     RecyclerView task_list;
-    TextView instruction,delete_all;
+    TextView instruction,delete_all, time_countdown;
     NumberPicker pickMM,pickHH,pickSS;
     List<task_data> all_data=new ArrayList<>();
     CustomAdapter adapter;
@@ -43,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.Ite
         FloatingActionButton fab = findViewById(R.id.fab);
         task_list = findViewById(R.id.task_list);
         instruction = findViewById(R.id.instruction);
+        time_countdown=findViewById(R.id.time_countdown);
         delete_all=findViewById(R.id.delete_all);
         detail_popup = new Dialog(MainActivity.this);
 
@@ -54,6 +57,7 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.Ite
         task_list.setAdapter(adapter);
         adapter.setClickListener(this);
 
+
         delete_all.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -64,13 +68,13 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.Ite
                 Toast toast =Toast.makeText(MainActivity.this, "All Tasks Deleted Successfully", Toast.LENGTH_SHORT);
                 toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 150);
                 toast.show();
+                delete_all.setVisibility(View.GONE);
                 instruction.setVisibility(View.VISIBLE);
             }
         });
 
         //Popup functionality and floating buttons
         fab.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onClick(View view) {
 
@@ -110,10 +114,10 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.Ite
                 detail_popup.show();
                 cancel_button.setOnClickListener(view1 -> detail_popup.dismiss());
                 save_button.setOnClickListener(new View.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.N)
                     @Override
                     public void onClick(View view) {
                         //Room database setup
-                        detail_popup.dismiss();
                         task_data input_data = new task_data();
                         input_data.setTask_name(popup_task_name.getText().toString().trim());
                         input_data.setHH(pickHH.getValue());
@@ -121,15 +125,25 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.Ite
                         input_data.setSS(pickSS.getValue());
                         adapter.notifyItemInserted(all_data.size());
                         all_data.add(input_data);
-                        if(!all_data.isEmpty())
-                        {
-                            instruction.setVisibility(View.GONE);
+                        if(seconds_to_plan()<0){
+
+                            all_data.remove(all_data.size()-1);
+                            adapter.notifyItemRemoved(all_data.size()-1);
+                            Toast.makeText(getApplicationContext(), "Only "+time_to_plan()+" remaining", Toast.LENGTH_SHORT).show();
                         }
-                        add_task st1 = new add_task(input_data);
-                        st1.start();
-                        Toast toast =Toast.makeText(MainActivity.this, "Task Added Successfully", Toast.LENGTH_SHORT);
-                        toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 150);
-                        toast.show();
+                        else {
+                            detail_popup.dismiss();
+                            if(!all_data.isEmpty())
+                            {
+                                instruction.setVisibility(View.GONE);
+                                delete_all.setVisibility(View.VISIBLE);
+                            }
+                            add_task st1 = new add_task(input_data);
+                            st1.start();
+                            Toast toast = Toast.makeText(MainActivity.this, "Task Added Successfully", Toast.LENGTH_SHORT);
+                            toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 150);
+                            toast.show();
+                        }
                     }
                 });
 
@@ -148,28 +162,53 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.Ite
         adapter.notifyItemRemoved(position);
         if(all_data.isEmpty())
         {
+            delete_all.setVisibility(View.GONE);
             instruction.setVisibility(View.VISIBLE);
         }
         del_task st1 = new del_task(del);
         st1.start();
 
+
+    }
+
+    int seconds_to_plan(){
+        int seconds = 0;
+        for(int i=0;i<all_data.size();i++) {
+            seconds = seconds + (all_data.get(i).getHH() * 3600) + (all_data.get(i).getMM() * 60) + (all_data.get(i).getSS());
+        }
+        seconds=86400-seconds;
+        return seconds;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    String time_to_plan(){
+        int seconds = 0,HH,MM, SS;
+        seconds=seconds_to_plan();
+        SS = seconds % 60;
+        HH = seconds / 60;
+        MM = HH % 60;
+        HH = HH / 60;
+        DecimalFormat formatter = new DecimalFormat("00");
+        return formatter.format(HH)+":"+formatter.format(MM)+":"+formatter.format(SS);
     }
 
 
 
-
     class get_all extends Thread {
+        @RequiresApi(api = Build.VERSION_CODES.N)
         public void run() {
             List <task_data> data;
             task_data_database db1 = Room.databaseBuilder(getApplicationContext(), task_data_database.class, "Plan24data").build();
             task_data_dao = db1.task_data_dao();
             data= task_data_dao.getAll();
             all_data.addAll(data);
-            data.clear();
             if(all_data.isEmpty())
             {
+                delete_all.setVisibility(View.GONE);
                 instruction.setVisibility(View.VISIBLE);
             }
+            data.clear();
+            time_countdown.setText(time_to_plan());
         }
     }
 
@@ -178,14 +217,20 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.Ite
         add_task(task_data data3) {
             this.data3=data3;
         }
+        @RequiresApi(api = Build.VERSION_CODES.N)
         public void run() {
             task_data_dao.insert(data3);
+            time_countdown.setText(time_to_plan());
+
         }
     }
 
     class delete_all extends Thread {
+        @RequiresApi(api = Build.VERSION_CODES.N)
         public void run() {
             task_data_dao.deleteAll();
+            time_countdown.setText(time_to_plan());
+
         }
     }
 
@@ -194,14 +239,18 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.Ite
         del_task(task_data data3) {
             this.data3=data3;
         }
+        @RequiresApi(api = Build.VERSION_CODES.N)
         public void run() {
             task_data_dao.delete(data3);
+            time_countdown.setText(time_to_plan());
         }
     }
 
 
 }
 
+//TO DO
 //Refresh after adding or deletion
 //Swipe features if possible
 //UI Enhancement
+//Order change on long press Recycler View
